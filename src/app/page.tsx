81cc8d8102a8b49a8349562e68a5e643b9bda6f3
Austin Chiatto <chiattoaustin@gmail.com>
2024-09-10 13:03:47 -0700

    implement SideBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import SideBar from '@/components/SideBar/SideBar';
+import { portfolioData } from '@/data/content';
+
 export default function Home() {
   return (
     <main className="grid grid-cols-12">
-      <aside className="col-span-12 lg:col-span-4 xl:col-span-3 p-4 md:p-6 lg:min-h-screen"></aside>
+      <SideBar content={portfolioData} />
       <section className="col-span-12 lg:col-span-8 xl:col-span-9 p-4 md:p-6 pl-0 md:pl-0 lg:h-screen lg:max-h-screen lg:overflow-y-scoll"></section>
     </main>
   );
