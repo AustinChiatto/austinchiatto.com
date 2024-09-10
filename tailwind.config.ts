3f2d4a958cf119d18595aa97b864c337de56c51c
Austin Chiatto <chiattoaustin@gmail.com>
2024-09-10 12:00:23 -0700

    implement base global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        transparent: 'transparent',
+        current: 'currentColor',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        'secondary-background': 'hsl(var(--secondary-background))',
+        'secondary-foreground': 'hsl(var(--secondary-foreground))'
       },
-    },
+      fontSize: {
+        sm: '16px'
+      },
+      fontWeight: {
+        normal: '400'
+      },
+      borderRadius: {
+        sm: '0.5rem',
+        md: '0.625rem',
+        lg: '1.125rem'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
