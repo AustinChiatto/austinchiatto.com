bb82fb1a44e27187740a8c30b4db9312f945455d
Austin Chiatto <chiattoaustin@gmail.com>
2024-09-10 12:45:28 -0700

    implement initial page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>landing</div>;
+  return (
+    <main className="grid grid-cols-12">
+      <aside className="col-span-12 lg:col-span-4 xl:col-span-3 p-4 md:p-6 lg:min-h-screen"></aside>
+      <section className="col-span-12 lg:col-span-8 xl:col-span-9 p-4 md:p-6 pl-0 md:pl-0 lg:h-screen lg:max-h-screen lg:overflow-y-scoll"></section>
+    </main>
+  );
 }
